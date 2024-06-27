2d9376fbc09ea15e92acb75d3c9899a7baedcfa3
Saurabh Rawat <yores.2002@gmail.com>
2024-06-27 20:15:25 +0530

    url_scraper

--- HTML_scraper/amazon_scraper.py
+++ HTML_scraper/amazon_scraper.py
@@ -5,5 +5,8 @@ import bs4
 
 
 if __name__=="__main__":
-    with open('amazon_products_urls.csv',newline=',') as csvfile:
-        
+    with open('amazon_products_urls.csv',newline='') as csvfile:
+        reader=csv.reader(csvfile,delimiter=',')
+        for row in reader:
+            url=row[0]
+            print(url)
