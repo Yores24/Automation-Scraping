7e7464062bb3b2b61c1089656ef428a90c78112c
Saurabh Rawat <yores.2002@gmail.com>
2024-06-29 13:25:33 +0530

    scraper

--- HTML_scraper/amazon_scraper.py
+++ HTML_scraper/amazon_scraper.py
@@ -4,9 +4,26 @@ import csv
 import bs4
 
 
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+REQUEST_HEADER={
+    'User-Agent':USER_AGENT,
+    'Accept-Language':'en-US, en;q=0.5',
+}
+
+def get_page_html(url):
+    res=requests.get(url=url,headers=REQUEST_HEADER)
+    return res.content
+
+def extract_product_info(url):
+    product_inf={}
+    print(f"Scrapping url: {url}" )
+    html=get_page_html(url)
+    print(html)
+
+
 if __name__=="__main__":
     with open('amazon_products_urls.csv',newline='') as csvfile:
         reader=csv.reader(csvfile,delimiter=',')
         for row in reader:
             url=row[0]
-            print(url)
+            print(extract_product_info(url))
